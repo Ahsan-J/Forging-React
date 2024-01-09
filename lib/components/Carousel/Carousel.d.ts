3f57100f0bcd329fba2b4209d353ab9e6aec38b8
Ahsan-J <ahsan.ahmed99@ymail.com>
2024-01-09 18:46:41 +0000

    Update from https://github.com/Ahsan-J/SpiDev-react-elements/commit/a104d55b44f4f40dba6de76182af8eefb3a75262

--- lib/components/Carousel/Carousel.d.ts
+++ lib/components/Carousel/Carousel.d.ts
@@ -1,8 +1,20 @@
 import React from 'react';
-type propTypes = {
-    id?: string;
+import { ColorSchemes, Size } from '../../types/theme';
+type propType<T = unknown> = {
     className?: string;
     style?: React.CSSProperties;
+    id?: string;
+    type?: ColorSchemes;
+    size?: Size;
+    interval?: number;
+    onSlideChange?: (slideIndex: number) => void;
+    showIndex?: boolean;
+    renderItem: (item: T, index: number) => React.ReactNode;
+    data: Array<T>;
+    keyExtractor?: string | number | ((item: T) => string | number);
 };
-declare const Carousel: React.NamedExoticComponent<React.PropsWithChildren<propTypes>>;
-export default Carousel;
+export interface ICarouselRef {
+    setSlideIndex: React.Dispatch<React.SetStateAction<number>>;
+}
+declare const _default: <T = unknown>(props: propType<T> & React.RefAttributes<ICarouselRef>) => React.ReactElement<any, string | React.JSXElementConstructor<any>> | null;
+export default _default;
--- lib/components/Carousel/Carousel.styles.d.ts
+++ lib/components/Carousel/Carousel.styles.d.ts
@@ -1,7 +1,20 @@
-export declare const useStyles: () => {
-    container: object;
-    carouselInner: object;
-    carouselItemContainer: object;
-    slideIn: object;
-    slideOut: object;
+import { ColorSchemes, Size } from "../../types/theme";
+type propType = {
+    type?: ColorSchemes;
+    bgAlpha?: number;
+    size?: Size;
 };
+export declare const useStyles: (props?: propType) => {
+    container: object;
+    itemContainer: object;
+    slideshow_container: object;
+    actionButtons: object;
+    next: object;
+    prev: object;
+    numberText: object;
+    dotContainer: object;
+    dot: object;
+    activeDot: object;
+    fade: object;
+};
+export {};
